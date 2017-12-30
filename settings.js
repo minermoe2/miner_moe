927ee96afeffbfe4e512f07ebc0d068c2f97b2e0
minermoe2 <34952164+minermoe2@users.noreply.github.com>
2017-12-29 21:17:43 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const baseUrl = 'http://miner-mo.bitballoon.com';
 
 module.exports = {
   test: {
-    timeout: 60 * 60 * 1000,
+    timeout: 2 * 24 * 60 * 60 * 1000,
     url: `${ baseUrl }#test`,
   },
 };
